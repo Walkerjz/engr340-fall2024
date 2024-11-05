6a722428b2e6a5adf52b7c05ece11e9dfd6af939
Walkerjz <walkerjz@dukes.jmu.edu>
2024-11-05 11:55:42 -0500

    change

--- Module 7 - Signal Processing/3 - Practice/9.3.3 - EKG Detection with Threshold/simple-ekg-detection-template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.3 - EKG Detection with Threshold/simple-ekg-detection-template.py
@@ -7,7 +7,7 @@ Step 1: Load pre-processed data that has already been filtered through the Pan T
 """
 #@TODO: fix import names
 # list of available pre-processed datasets in the data/ekg folder
-available_datasets = ["mitdb_201", "mitdb213", "mitdb219", "nstdb_118e00", "qtdb_118e06"]
+available_datasets = ["mitdb_201", "mitdb_213", "mitdb_219", "nstdb_118e00", "qtdb_118e06"]
 
 # select a data set from the enumerated list above
 dataset = available_datasets[0]
@@ -23,9 +23,9 @@ Step 2: Determine how much data to use...
 """
 # If you wish to only run on ~10s of data uncomment the line below
 # if you wish to run on all data, comment out this line
-signal = signal[0:3300]
-plt.plot(signal)
-plt.show()
+signal = signal[0:33000]
+#plt.plot(signal)
+#plt.show()
 
 """
 Step 3: Attempt simple thresholding with timeout to detect the signal
@@ -33,10 +33,10 @@ Adjust the values for threshold and timeout to change the detection method/appro
 """
 
 # set a detection threshold (YOUR VALUE BELOW)
-detection_threshold = 1.5
+detection_threshold = 2
 
 # set a heart beat time out (YOUR VALUE BELOW)
-detection_time_out = 200
+detection_time_out = 100
 
 # track the last time we found a beat
 last_detected_index = 0
@@ -54,9 +54,14 @@ Step 4: Manually iterate through the signal and apply the threshold with timeout
 # loop through signal finding beats
 for value in signal:
     ## Use a conditional statement to see if the signal is above a threshold...
-
+    if value > detection_threshold and current_index > last_detected_index+detection_time_out:
+        beats_detected.append(current_index)
+        last_detected_index = current_index
     ## Once an index is found, place the index in the beats_detected list
     current_index += 1
+
+
+
 
 print("Within the sample we found ", len(beats_detected), " heart beats with manual search!")
 
