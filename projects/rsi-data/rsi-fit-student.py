bba30d52c56c8eb3c9455d03fab3da4b238a4268
Walkerjz <walkerjz@dukes.jmu.edu>
2024-11-16 18:36:53 -0500

    assessment 2 90%

--- projects/rsi-data/rsi-fit-student.py
+++ projects/rsi-data/rsi-fit-student.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 Preamble: Load data from source CSV file
 """
 path = "../../data/drop-jump/all_participant_data_rsi.csv"
-#data = np.loadtxt(path, delimiter=',', skiprows=1)
+#put the data in a data frame
 df = pd.read_csv(path)
 """
 Question 1: Load the force plate and acceleration based RSI data for all participants. Map each data set (accel and FP)
@@ -16,38 +16,47 @@ to a normal distribution. Clearly report the distribution parameters (mu and std
 probability distribution function. Include appropriate labels, titles, and legends.
 """
 print('-----Question 1-----')
-
+#put the Force Plate and accelerometer RSI values in different arrays
 FPdata = df['force_plate_rsi'].to_numpy()
 acceldata = df['accelerometer_rsi'].to_numpy()
 
+#make a histogram plot of the Force plate data
 plt.hist(FPdata, bins = 'auto', label = 'Force Plate RSI data')
 plt.xlabel('Relative Force Plate RSI')
 plt.ylabel('Counts')
 plt.title('Force Plate Data Histogram')
 plt.show()
+#calculate statistics to fit a normal to the force plate RSI data
 (FP_fitted_mean, FP_fitted_std) = norm.fit(FPdata)
+#generate title for normal and add and also print the calculated statistics
 plt.title('Distribution of Force Plate RSI' + 'with mu =' + str(FP_fitted_mean) + 'and std =' + str(FP_fitted_std))
 print('Force Plate RSI' + ' has mu =' + str(FP_fitted_mean) + 'and std =' + str(FP_fitted_std))
+#generate the actual normal curve with x and y
 x = np.linspace(start = -0.25, stop = 1.5, num = 10000)
 y = norm.pdf(x, loc=FP_fitted_mean, scale=FP_fitted_std)
+#plot it all
 plt.plot(x,y,label = 'normal')
 plt.legend()
 plt.show()
 
+#make a histogram plot of the accelerometer data
 plt.hist(acceldata, bins = 15, label = 'Accelerometer RSI data')
 plt.xlabel('Accelerometer Plate RSI')
 plt.ylabel('Counts')
 plt.title('Accelerometer Data Histogram')
 plt.show()
+#calculate statistics to fit a normal to the accelerometer RSI data
 (acl_fitted_mean, acl_fitted_std) = norm.fit(acceldata)
+#generate title for normal and add and also print the calculated statistics
 plt.title('Distribution of Accelerometer RSI' + 'with mu =' + str(acl_fitted_mean) + 'and std =' + str(acl_fitted_std))
 print('Accelerometer RSI' + ' has mu =' + str(acl_fitted_mean) + 'and std =' + str(acl_fitted_std))
+#generate the actual normal curve with x and y
 x = np.linspace( start = -0.25, stop = 1.5, num = 10000)
 y = norm.pdf(x, loc=acl_fitted_mean, scale= acl_fitted_std)
+#plot it all
 plt.plot(x,y,label = 'normal')
 plt.legend()
 plt.show()
-
 
 """
 Question 2: Conduct a Chi2 Goodness of Fit Test for each dataset to test whether the data is a good fit
@@ -60,12 +69,14 @@ print('\n\n-----Question 2-----')
 """
 Acceleration
 """
-
-
+#generate 9 bins between [0,2)
 bins = np.linspace(0, 2, 9)
+#append -inf and +inf to both ends of the bins
 bins = np.r_[-np.inf, bins, np.inf]
+#expected statistics
 expected_mu = 0
 expected_std = 1
+#find observed counts in each bin
 observed_counts, observed_edges = np.histogram(acceldata, bins=bins, density=False)
 
 """UNSURE OF HOW TO DO EXPECTED ON THIS"""
@@ -74,9 +85,12 @@ expected_prob = np.diff(thing)
 expected_counts = expected_prob * len(acceldata)
 """UNSURE OF HOW TO DO EXPECTED ON THIS"""
 
+#Generate and print chisquare statistics
 (chi_stat, p_value) = chisquare(f_obs=observed_counts, f_exp=expected_counts, ddof=2)
 print('Chi2 stat: ', chi_stat, 'p-value: ', p_value)
+#define alpha
 a = 0.05
+#compare alpha to p value to determine if the null hypothesis should be chosen
 if p_value < a:
     print('Reject null hypothesis. bad fit.')
 else:
@@ -86,10 +100,14 @@ else:
 """
 Force Plate
 """
+#generate 9 bins between [0,2)
 bins = np.linspace(0, 2, 9)
+#append -inf and +inf to both ends of the bins
 bins = np.r_[-np.inf, bins, np.inf]
+#expected statistics
 expected_mu = 0
 expected_std = 1
+#find observed counts in each bin
 observed_counts, observed_edges = np.histogram(FPdata, bins=bins, density=False)
 
 """UNSURE OF HOW TO DO EXPECTED ON THIS"""
@@ -97,10 +115,12 @@ thing = norm.cdf(bins, loc=expected_mu, scale=expected_std)
 expected_prob = np.diff(thing)
 expected_counts = expected_prob * len(FPdata)
 """UNSURE OF HOW TO DO EXPECTED ON THIS"""
-
+#Generate and print chisquare statistics
 (chi_stat, p_value) = chisquare(f_obs=observed_counts, f_exp=expected_counts, ddof=2)
 print('Chi2 stat: ', chi_stat, 'p-value: ', p_value)
+#define alpha
 a = 0.05
+#compare alpha to p value to determine if the null hypothesis should be chosen
 if p_value < a:
     print('Reject null hypothesis. bad fit.')
 else:
@@ -112,18 +132,13 @@ or not. Clearly report the p-value for the t-test and make a clear determination
 An alpha=0.05 is suitable for these tests.
 """
 print('\n\n-----Question 3-----')
+#define alpha
 A = 0.05
+#calculate test statistics
 (stat, p_value) = ttest_ind(FPdata,acceldata,alternative='two-sided')
-### see hypothesis-test file
-
-'''CHECK IF IF STATEMENT IS CORRECT'''
-'''
-DO THIS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-Comments are effectively used to convey the approach and describe operations conducted.
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-'''
-
-
+### print the P-value
+print('the p-value is: ' + str(p_value) +' and alpha is: 0.05')
+### see if the RSI values from the force plate and accelerometer are equal
 if p_value < A:
     print('Reject H0: RSI means for the acceleration and force plate data are NOT equivalent')
 else:
@@ -136,16 +151,20 @@ plot a histogram of the data on the same plot showing the fitted normal curve. I
 legends. The default binning approach from matplot lib with 16 bins is sufficient.
 """
 
-
-'''ADD AXIS TITIELS'''
-
-RSIerror = abs(acceldata- FPdata)
+### calculate error between force plate and accelerometer RSI values, didn't absolute value it so the data set is more symmetric
+RSIerror = acceldata- FPdata
+### calculate mean and std to fit a normal distribution
 (Er_fitted_mean, Er_fitted_std) = norm.fit(RSIerror)
+### plot a histogram, density is to show what percentage of value in a bin rather than counts to make the graph scaling better
 plt.hist(RSIerror,bins = 16, label = 'RSI error data', density=True)
+### calculate the x and y values of a normal curve fitted to the RSI error
 x = np.linspace(start = -1, stop = 1, num = 10000)
 y = norm.pdf(x, loc = Er_fitted_mean, scale = Er_fitted_std)
+### plot the normal on the same graph
 plt.plot(x,y,label = 'normal')
 plt.title("RSI error")
+plt.ylabel("Density")
+plt.xlabel('RSI measurement error')
 plt.legend()
 plt.show()
 
